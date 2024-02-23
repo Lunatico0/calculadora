0d145e457e6fb3d40f6c549800e0601f1b6a9abc
Patricio Pittana <pittanapatricio@gmail.com>
2024-02-23 20:11:08 -0300

    hice las operaciones en funciones

--- js/main.js
+++ js/main.js
@@ -7,31 +7,51 @@ option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Dividir.\n4. 
 while (option.toLowerCase() !== "salir") {
     switch (option) {
         case "1":
-            num1 = Number(prompt("Ingrese el 1° sumando"));
-            num2 = Number(prompt("Ingrese el 2° sumando"));
-            console.log(resultado = num1 + num2);
-            alert("La suma de " + num1 + " + " + num2 + " = " + resultado);
+            suma();
             break;
         case "2":
-            num1 = Number(prompt("Ingrese el minuendo"));
-            num2 = Number(prompt("Ingrese el sustraendo"));
-            console.log(resultado = num1 - num2);
-            alert("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
+            resta();
             break;
         case "3":
-            num1 = Number(prompt("Ingrese el dividendo"));
-            num2 = Number(prompt("Ingrese el divisor"));
-            console.log(resultado = num1 / num2);
-            alert("El cociente de " + num1 + " / " + num2 + " = " + resultado);
+            division();
             break;
         case "4":
-            num1 = Number(prompt("Ingrese el multiplicando"));
-            num2 = Number(prompt("Ingrese el multiplicador"));
-            console.log(resultado = num1 * num2);
-            alert("El producto de " + num1 + " * " + num2 + " = " + resultado);
+            multiplicacion();
             break;
         default:
             alert("Opción no válida. Inténtalo de nuevo.");
     }
     option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Dividir.\n4. Multiplicar.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
+}
+
+function suma(){
+    num1 = Number(prompt("Ingrese el 1° sumando"));
+    num2 = Number(prompt("Ingrese el 2° sumando"));
+    resultado = num1 + num2;
+    console.log("La suma de " + num1 + " + " + num2 + " = " + resultado);
+    alert("La suma de " + num1 + " + " + num2 + " = " + resultado);
+}
+
+function resta(){
+    num1 = Number(prompt("Ingrese el minuendo"));
+    num2 = Number(prompt("Ingrese el sustraendo"));
+    resultado  = num1 - num2;
+    console.log("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
+    alert("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
+}
+
+function division(){
+    num1 = Number(prompt("Ingrese el dividendo"));
+    num2 = Number(prompt("Ingrese el divisor"));
+    resultado = num1 / num2;
+    console.log("El cociente de " + num1 + " / " + num2 + " = " + resultado);
+    alert("El cociente de " + num1 + " / " + num2 + " = " + resultado);
+}
+
+function multiplicacion(){
+    num1 = Number(prompt("Ingrese el multiplicando"));
+    num2 = Number(prompt("Ingrese el multiplicador"));
+    resultado = num1 * num2;
+    console.log("El producto de " + num1 + " * " + num2 + " = " + resultado);
+    alert("El producto de " + num1 + " * " + num2 + " = " + resultado);
 }
