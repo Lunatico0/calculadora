3db23748ab5fee645b3d7e6e759db20f28b6d78d
Patricio Pittana <pittanapatricio@gmail.com>
2024-02-25 02:56:49 -0300

    Algo de estilado css, reemplazo de las operaciones por funciones que hagan dichas operaciones, adicion de fincionalidad ANS, validador de datos ingresado por el usuario

--- js/main.js
+++ js/main.js
@@ -1,57 +1,131 @@
 let option = null;
 let resultado;
-let num1, num2;
+let ans;
 
-option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Dividir.\n4. Multiplicar.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
+option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
 
 while (option.toLowerCase() !== "salir") {
     switch (option) {
         case "1":
-            suma();
+            ans = suma();
             break;
         case "2":
-            resta();
+            ans = resta();
             break;
         case "3":
-            division();
+            ans = multiplicacion();
             break;
         case "4":
-            multiplicacion();
+            ans = division();
+            break;
+        case "5":
+            option = prompt('¿Qué deseas hacer con ' + ans + '?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
+            while (option.toLowerCase() !== "salir") {
+                switch (option) {
+                    case "1":
+                        ans = sumaAns(ans);
+                        break;
+                    case "2":
+                        ans = restaAns(ans);
+                        break;
+                    case "3":
+                        ans = multiplicacionAns(ans);
+                        break;
+                    case "4":
+                        ans = divisionAns(ans);
+                        break;
+                    default:
+                        alert("Opción no válida. Inténtalo de nuevo.");
+                }
+                option = prompt('¿Qué deseas hacer con ' + ans + '?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
+            }
             break;
         default:
             alert("Opción no válida. Inténtalo de nuevo.");
     }
-    option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Dividir.\n4. Multiplicar.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
+    option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n5. ANS.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
+}
+
+
+function sumaAns(ans){
+    num2 = pedirNumero("Ingrese el 2° sumando");
+    resultado = ans + num2;
+    console.log("La suma de " + ans + " + " + num2 + " = " + resultado);
+    alert("La suma de " + ans + " + " + num2 + " = " + resultado);
+    return resultado;
+}
+
+function restaAns(ans){
+    num2 = pedirNumero("Ingrese el sustraendo");
+    resultado  = ans - num2;
+    console.log("La diferencia de " + ans + " - " + num2 + " = " + resultado);
+    alert("La diferencia de " + ans + " - " + num2 + " = " + resultado);
+    return resultado;
+}
+
+function divisionAns(ans){
+    num2 = pedirNumero("Ingrese el dividendo");
+    resultado = ans / num2;
+    console.log("El cociente de " + ans + " / " + num2 + " = " + resultado);
+    alert("El cociente de " + ans + " / " + num2 + " = " + resultado);
+    return resultado;
+}
+
+function multiplicacionAns(ans){
+    num2 = pedirNumero("Ingrese el multiplicador");
+    resultado = ans * num2;
+    console.log("El producto de " + ans + " * " + num2 + " = " + resultado);
+    alert("El producto de " + ans + " * " + num2 + " = " + resultado);
+    return resultado;
 }
 
 function suma(){
-    num1 = Number(prompt("Ingrese el 1° sumando"));
-    num2 = Number(prompt("Ingrese el 2° sumando"));
+    let num1, num2;
+    num1 = pedirNumero("Ingrese el 1° sumando");
+    num2 = pedirNumero("Ingrese el 2° sumando");
     resultado = num1 + num2;
     console.log("La suma de " + num1 + " + " + num2 + " = " + resultado);
     alert("La suma de " + num1 + " + " + num2 + " = " + resultado);
+    return resultado;
 }
 
 function resta(){
-    num1 = Number(prompt("Ingrese el minuendo"));
-    num2 = Number(prompt("Ingrese el sustraendo"));
+    let num1, num2;
+    num1 = pedirNumero("Ingrese el minuendo");
+    num2 = pedirNumero("Ingrese el sustraendo");
     resultado  = num1 - num2;
     console.log("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
     alert("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
+    return resultado;
 }
 
 function division(){
-    num1 = Number(prompt("Ingrese el dividendo"));
-    num2 = Number(prompt("Ingrese el divisor"));
+    let num1, num2;
+    num1 = pedirNumero("Ingrese el divisor");
+    num2 = pedirNumero("Ingrese el dividendo");
     resultado = num1 / num2;
     console.log("El cociente de " + num1 + " / " + num2 + " = " + resultado);
     alert("El cociente de " + num1 + " / " + num2 + " = " + resultado);
+    return resultado;
 }
 
 function multiplicacion(){
-    num1 = Number(prompt("Ingrese el multiplicando"));
-    num2 = Number(prompt("Ingrese el multiplicador"));
+    let num1, num2;
+    num1 = pedirNumero("Ingrese el multiplicando");
+    num2 = pedirNumero("Ingrese el multiplicador");
     resultado = num1 * num2;
     console.log("El producto de " + num1 + " * " + num2 + " = " + resultado);
     alert("El producto de " + num1 + " * " + num2 + " = " + resultado);
+    return resultado;
+}
+
+function pedirNumero(mensaje) {
+    let input;
+    do {
+        input = Number(prompt(mensaje));
+        if (isNaN(input)) {
+            alert("Por favor, ingresa un número válido.");
+        }
+    } while (isNaN(input));
+    return input;
 }
